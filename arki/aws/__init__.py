645f771412afd6f438165fd49d94a1ea3bc0dbeb
kyhau <virtualda@gmail.com>
2018-08-28 19:17:32 +1000

    Added unit tests

--- arki/aws/__init__.py
+++ arki/aws/__init__.py
@@ -14,6 +14,7 @@ class DefaultEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return int(mktime(obj.timetuple()))
+
         return json.JSONEncoder.default(self, obj)
 
 
