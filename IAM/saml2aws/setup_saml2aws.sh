8a3378e3c7dc74addd7badad3a36edaee0407c8f
Kay Hau <virtualda@gmail.com>
2019-09-08 11:13:18 +1000

    Update setup_saml2aws.sh

--- IAM/saml2aws/setup_saml2aws.sh
+++ IAM/saml2aws/setup_saml2aws.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-cd ~
-wget https://github.com/Versent/saml2aws/releases/download/v2.16.0/saml2aws_2.16.0_linux_amd64.tar.gz
-tar xfz saml2aws_2.16.0_linux_amd64.tar.gz
+VERSION=2.16.0
+ZIP_FILE="https://github.com/Versent/saml2aws/releases/download/v${VERSION}/saml2aws_${VERSION}_linux_amd64.tar.gz"
+
+# Download zip to home directory
+pushd ~
+[ -f ${ZIP_FILE} ] || wget ${ZIP_FILE}
+tar xfz saml2aws_${VERSION}_linux_amd64.tar.gz
+rm saml2aws_${VERSION}_linux_amd64.tar.*
+
+# Copy binary to .local/bin and create a symlink for bookmarking the version
 mkdir -p .local/bin
-cp saml2aws .local/bin/
+rm .local/bin/saml2aws || true
+rm .local/bin/saml2aws-v${VERSION} || true
+
+mv saml2aws .local/bin/
+pushd .local/bin
+ln -s saml2aws saml2aws-v${VERSION}
+popd
+
+popd
 
 # Configure saml2aws
 # saml2aws configure
