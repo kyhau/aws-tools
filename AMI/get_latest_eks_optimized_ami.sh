363b620f12093f69673c1c873ad4701fe3bd426f
Kay Hau <virtualda@gmail.com>
2019-09-21 11:04:49 +1000

    Added get_latest_eks_optimized_ami.sh

---
+++ AMI/get_latest_eks_optimized_ami.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Return the latest ami-id of the EKS Optimised Amazon Linux 2 AMI
+# Assume region ap-southeast-2
+# https://docs.aws.amazon.com/eks/latest/userguide/eks-optimized-ami.html
+
+latest_ami=$(aws ssm get-parameters --names aws ssm get-parameters --names /aws/service/eks/optimized-ami/1.14/amazon-linux-2/recommended/image_id --region ap-southeast-2 --query "Parameters[0].Value" | awk -F \" '{print $2}')
+echo "$latest_ami"
