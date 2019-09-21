2c68ac5320936e7dd9c009c3e4c6cb25798d4e5c
Kay Hau <virtualda@gmail.com>
2019-09-21 11:04:28 +1000

    Added list_stacks.py

---
+++ CloudFormation/list_stacks.py
@@ -0,0 +1,28 @@
+from boto3.session import Session
+import click
+
+from arki_common.utils import print_json
+
+
+@click.command()
+@click.option("--profile", "-p", help="AWS profile name")
+@click.option("--name-contains", "-n", help="Find stacks with the given string in the name")
+def main(profile, name_contains):
+    session = Session(profile_name=profile)
+
+    result = []
+
+    paginator = session.client("cloudformation").get_paginator("describe_stacks")
+    for page in paginator.paginate():
+        for stack in page["Stacks"]:
+            if name_contains is None or name_contains in stack["StackName"]:
+                #print_json(page)
+                result.append((stack["StackName"], stack["CreationTime"]))
+
+    # Sort by creation time in reverse order
+    result.sort(key=lambda tup: tup[1], reverse=True)
+    for r in result:
+        print(r[0])
+
+
+if __name__ == "__main__": main()
