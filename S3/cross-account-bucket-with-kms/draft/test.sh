6cc18a4e5f086f91cc5df73ed41566d712d72be6
Kay Hau <virtualda@gmail.com>
2019-10-02 07:06:14 +1000

    Updated draft

--- S3/cross-account-bucket-with-kms/draft/test.sh
+++ S3/cross-account-bucket-with-kms/draft/test.sh
@@ -9,16 +9,19 @@ function upload_file {
 function download_file {
   # Ref: https://aws.amazon.com/premiumsupport/knowledge-center/decrypt-kms-encrypted-objects-s3/
 
-  ROLE_ARN="arn:aws:iam::111111111111:role/s3-bucket-readonly-role"
+  ROLE_ARN="arn:aws:iam::111111111111:role/k-shared-cross-account-bucket-readonly-role"
 
   resp=$(aws sts assume-role --role-arn ${ROLE_ARN} --profile account-c --role-session-name test-s3)
   export AWS_ACCESS_KEY_ID=$(echo "$resp" | jq -r '.Credentials.AccessKeyId')
   export AWS_SECRET_ACCESS_KEY=$(echo "$resp" | jq -r '.Credentials.SecretAccessKey')
   export AWS_SESSION_TOKEN=$(echo "$resp" | jq -r '.Credentials.SessionToken')
 
-  aws s3 ls s3://k-shared-cross-account-bucket
+  #aws s3 ls s3://k-shared-cross-account-bucket
+
+  #aws kms describe-key --key-id=xxx
 
   aws s3 cp s3://k-shared-cross-account-bucket/account_b.json test_download.json
+  #aws s3api get-object --bucket k-shared-cross-account-bucket --key account_b.json test_download.json
 }
 
 #upload_file
