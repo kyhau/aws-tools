5dd8384142b3661518e4f9b3541b553aca9d8c9d
Kay Hau <virtualda@gmail.com>
2019-10-14 12:37:00 +1100

    Updated glacier_to_s3.sh

--- S3/glacier_to_s3.sh
+++ S3/glacier_to_s3.sh
@@ -14,14 +14,31 @@ GLACIER_RESTORE_LIST="glacier-restore.txt"
 
 [[ -f "$GLACIER_RESTORE_LIST" ]] && rm "$GLACIER_RESTORE_LIST"
 
-for key_prefix in "${PREFIXES[@]}"
-do
-  aws s3 ls "s3://${BUCKET_NAME}/${key_prefix}" --recursive | \
-    awk '{print $4}' | sed 's#s3://${BUCKET_NAME}/##' >> "$GLACIER_RESTORE_LIST"
-done
+write_object_keys_to_file() {
+  for key_prefix in "${PREFIXES[@]}"
+  do
+    aws s3 ls "s3://${BUCKET_NAME}/${key_prefix}" --recursive | \
+      awk '{print $4}' | sed 's#s3://${BUCKET_NAME}/##' >> "$GLACIER_RESTORE_LIST"
+  done
+}
 
-while IFS= read -r item
-do
-  echo "Restoring $item"
-  aws s3api restore-object --bucket "$BUCKET_NAME" --key "$item" --restore-request Days="$DAYS"
-done < "$GLACIER_RESTORE_LIST"
+restore_objects() {
+  while IFS= read -r item
+  do
+    echo "Restoring $item"
+    #aws s3api restore-object --bucket "$BUCKET_NAME" --key "$item" --restore-request Days="$DAYS",GlacierJobParameters={Tier=Expedited}
+    aws s3api restore-object --bucket "$BUCKET_NAME" --key "$item" --restore-request Days="$DAYS"
+  done < "$GLACIER_RESTORE_LIST"
+}
+
+check_object_status() {
+  while IFS= read -r item
+  do
+    echo "Checking $item"
+    aws s3api head-object --bucket "$BUCKET_NAME" --key "$item" --query "Restore" --output text
+  done < "$GLACIER_RESTORE_LIST"
+}
+
+write_object_keys_to_file
+restore_objects
+check_object_status
