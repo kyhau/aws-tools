f6e34719fdca9c119be37e708124bda1a4b97ebb
Kay Hau <virtualda@gmail.com>
2019-10-27 13:15:25 +1100

    Added more templates

--- EC2-VPC/list_vpc_endpoint_services_supported.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set to fail script if any command fails.
-set -e
-
-awk -F' ' '{for (i = 0; ++i <= NF;) print $i}' <<< $(
-  aws ec2 describe-vpc-endpoint-services --query 'ServiceNames[*]' --output text
-)
