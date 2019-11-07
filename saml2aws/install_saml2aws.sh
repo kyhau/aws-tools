2774d881ddbfaf2320c412c7f79c65814271b4ab
Kay Hau <virtualda@gmail.com>
2019-11-07 16:10:46 +1100

    Updated saml2aws version

--- saml2aws/install_saml2aws.sh
+++ saml2aws/install_saml2aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.16.0
+VERSION=2.19.0
 ZIP_FILE="https://github.com/Versent/saml2aws/releases/download/v${VERSION}/saml2aws_${VERSION}_linux_amd64.tar.gz"
 
 echo "Downloading saml2aws_${VERSION}_linux_amd64.tar.gz to home directory..."
@@ -12,7 +12,7 @@ rm saml2aws_${VERSION}_linux_amd64.tar.*
 echo "Coping binary to .local/bin and create a symlink for bookmarking the version..."
 mkdir -p .local/bin
 rm .local/bin/saml2aws || true
-rm .local/bin/saml2aws-v${VERSION} || true
+rm .local/bin/saml2aws-v* || true
 
 mv saml2aws .local/bin/
 pushd .local/bin
