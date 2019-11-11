76653794b2428046e61bf5b467a6611d3cfa42ad
Kay Hau <virtualda@gmail.com>
2019-11-12 09:24:40 +1100

    Added install_awscli_v2.sh

---
+++ SSO/install_awscli_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# See https://aws.amazon.com/blogs/developer/aws-cli-v2-installers/
+# See https://aws.amazon.com/blogs/developer/aws-cli-v2-now-supports-aws-single-sign-on/
+
+curl 'https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip' -o 'awscli-exe.zip'
+
+unzip awscli-exe.zip
+
+# Install the AWS CLI v2 preview under the /usr/local/aws-cli and create an aws2 symlink in /usr/local/bin
+sudo ./aws/install
+
+rm -rf awscli-exe.zip ./aws
+
+aws2 --version
