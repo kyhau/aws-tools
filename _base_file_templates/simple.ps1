86a14d292fc76feed97297b2fb9a2044ebe547c8
Kay Hau <virtualda@gmail.com>
2019-11-26 14:28:39 +1100

    Added simple.ps1

---
+++ _base_file_templates/simple.ps1
@@ -0,0 +1,20 @@
+﻿# Enable logging
+
+# TODO Change appName, logroot
+$appName = "TODO"
+$logroot = "C:\temp\logs\$($appName)"
+$daysToKeep = -14
+If (!(Test-Path $logroot)) {New-Item -ItemType Directory -Force -Path $logroot}
+
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path "$($logroot)\$(get-date -f yyyyMMddHHmmss)-$($appName).txt" -append
+
+# Remove old log files
+# TODO change filter
+Get-ChildItem -Path $logroot -Filter "*-$($appName).txt" -Recurse -File | Where-Object LastWriteTime -le (Get-Date).AddDays($daysToKeep) | Remove-Item -Force
+
+Write-Output "Do something..."
+
+Stop-Transcript
