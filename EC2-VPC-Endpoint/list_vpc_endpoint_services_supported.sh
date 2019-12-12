a2a957220517c040ce7ad940a9e6cfbd15e69cf6
Kay Hau <virtualda@gmail.com>
2019-12-12 16:08:42 +1100

    Minor update on .sh to support additional arguments (e.g. profile)

--- EC2-VPC-Endpoint/list_vpc_endpoint_services_supported.sh
+++ EC2-VPC-Endpoint/list_vpc_endpoint_services_supported.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-# Set to fail script if any command fails.
-set -e
 
 # Define the help menu
 help_menu() {
   echo "Usage:
   ${0##*/}
-    --name-only    Name only
+    --profile      profile_name (optional)
+    --name-only    Name-only (optional)
   "
-  exit
+  return
 }
 
 # Parse arguments
 NAME_ONLY=false
+PROFILE=default
 
 while [[ "$#" > 0 ]]; do case $1 in
-    --name-only)         NAME_ONLY=true                          ;;
+    --profile)           PROFILE="${2}"                   ; shift ;;
+    --name-only)         NAME_ONLY=true                           ;;
     -h|--help)           help_menu                                ;;
     *)                   echo "Invalid option: ${1}" && help_menu ;;
 esac; shift; done
 
 if [[ $NAME_ONLY = true ]]; then
   awk -F' ' '{for (i = 0; ++i <= NF;) print $i}' <<< $(
-    aws ec2 describe-vpc-endpoint-services --query 'ServiceNames[*]' --output text
+    aws ec2 describe-vpc-endpoint-services --query 'ServiceNames[*]' --output text --profile $PROFILE
   )
 else
-  aws ec2 describe-vpc-endpoint-services
+  aws ec2 describe-vpc-endpoint-services --profile $PROFILE
 fi
+
+return
