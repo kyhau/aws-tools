81d688b0323f3436307ac5ea547fd313cc63c7be
Kay Hau <virtualda@gmail.com>
2020-03-03 11:39:15 +1100

    Updated get_inventory.sh

--- SSM/CustomInventory/get_inventory.sh
+++ SSM/CustomInventory/get_inventory.sh
@@ -2,4 +2,6 @@
 # Set to fail script if any command fails.
 set -e
 
-aws ssm get-inventory --filters '[{"Key":"Custom:DatabaseProcesses.Name","Values":["sqlservr"],"Type":"Equal"}]' --profile infra-prd-admin
+aws ssm get-inventory --filters '[{"Key":"Custom:DatabaseProcesses.Name","Values":["sqlservr"],"Type":"Equal"}]'
+
+aws ssm get-inventory --filters '[{"Key":"AWS:Service.DisplayName","Values":["Windows Firewall"],"Type":"Equal"},{"Key":"AWS:Service.Status","Values":["Stopped"],"Type":"Equal"},{"Key":"AWS:Tag.Key","Values":["env"],"Type":"Equal"},{"Key":"AWS:Tag.Value","Values":["Prod"],"Type":"Equal"}]'
