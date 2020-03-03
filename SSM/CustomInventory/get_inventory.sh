4eca54795987e923e279387e266c10d253704b92
Kay Hau <virtualda@gmail.com>
2020-03-03 12:12:32 +1100

    Removed old file

--- SSM/CustomInventory/get_inventory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set to fail script if any command fails.
-set -e
-
-aws ssm get-inventory --filters '[{"Key":"Custom:DatabaseProcesses.Name","Values":["sqlservr"],"Type":"Equal"}]'
-
-aws ssm get-inventory --filters '[{"Key":"AWS:Service.DisplayName","Values":["Windows Firewall"],"Type":"Equal"},{"Key":"AWS:Service.Status","Values":["Stopped"],"Type":"Equal"},{"Key":"AWS:Tag.Key","Values":["env"],"Type":"Equal"},{"Key":"AWS:Tag.Value","Values":["Prod"],"Type":"Equal"}]'
