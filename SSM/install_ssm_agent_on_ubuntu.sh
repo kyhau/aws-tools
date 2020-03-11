ac3039f62d989388a990c0522ef722cc326e3c29
kyhau <virtualda@gmail.com>
2020-03-11 12:06:49 +1100

    Updated ssm agent installation script

--- SSM/install_ssm_agent_on_ubuntu.sh
+++ SSM/install_ssm_agent_on_ubuntu.sh
@@ -8,7 +8,10 @@ lsb_release -a
 # Check 64 or 32
 uname -i
 
-# Install
+# Install snap
+sudo apt install snapd
+
+# Install amazon-ssm-agent using snap
 sudo snap install amazon-ssm-agent --classic
 
 # Or, udpate
