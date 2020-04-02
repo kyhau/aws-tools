186a2424d087e76a4050e9cf1ffda809cc3ff1de
kyhau <virtualda@gmail.com>
2020-04-03 10:19:42 +1100

    Minor refactoring

--- ACM/list_certificates.py
+++ ACM/list_certificates.py
@@ -21,8 +21,6 @@ try:
 except Exception as e:
     logging.error(e)
 
-accounts_processed = []
-
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
@@ -31,12 +29,7 @@ def json_serial(obj):
     raise TypeError ("Type %s not serializable" % type(obj))
 
 
-def list_action(session, aws_region):
-    account_id = session.client("sts").get_caller_identity()["Account"]
-    if account_id in accounts_processed:
-        return
-    accounts_processed.append(account_id)
-
+def process_account(session, account_id, aws_region):
     regions = session.get_available_regions("acm") if aws_region == "all" else [aws_region]
     for region in regions:
         logging.debug(f"Checking {account_id} {region}")
@@ -60,26 +53,36 @@ def list_action(session, aws_region):
                 raise
         except Exception as e:
             logging.error(e)
+            import traceback
+            traceback.print_exc()
 
 
 ################################################################################
 # Entry point
 
 @click.command()
-@click.option("--profile", "-p", help="AWS profile name")
-@click.option("--region", "-r", help="AWS Region; use 'all' for all regions", default="ap-southeast-2")
+@click.option("--profile", "-p", help="AWS profile name.")
+@click.option("--region", "-r", help="AWS Region; use 'all' for all regions.", default="ap-southeast-2")
 def main(profile, region):
+    accounts_processed = []
     profile_names = [profile] if profile else aws_profiles
     
     for profile_name in profile_names:
         try:
             session = Session(profile_name=profile_name)
-            list_action(session, region)
+            account_id = session.client("sts").get_caller_identity()["Account"]
+            if account_id in accounts_processed:
+                continue
+            accounts_processed.append(account_id)
+            
+            process_account(session, account_id, region)
 
         except ClientError as e:
+            error_code = e.response["Error"]["Code"]
             if e.response["Error"]["Code"] == "ExpiredToken":
-                logging.warning(f"{profile_name} token expired. Skipped")
+                logging.warning(f'{profile_name} {error_code}. Skipped')
             else:
                 raise
+
 
 if __name__ == "__main__": main()
