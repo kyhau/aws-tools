d836ee65fb9faa4880e57fcdcd22c9aa3c643d2c
kyhau <virtualda@gmail.com>
2020-04-03 11:07:00 +1100

    Minor refactoring

--- SSM/ssm_run_command.py
+++ SSM/ssm_run_command.py
@@ -26,22 +26,15 @@ try:
 except Exception as e:
     logging.error(e)
 
-accounts_processed = []
 
-
-def do_something(session, instanceid, aws_region, profile):
-    account_id = session.client("sts").get_caller_identity()["Account"]
-    if account_id in accounts_processed:
-        return
-    accounts_processed.append(account_id)
-
+def do_something(session, profile, aws_region, instanceid):
     regions = session.get_available_regions("ssm") if aws_region == "all" else [aws_region]
     for region in regions:
-        logging.debug(f"Checking {account_id} {region}")
+        logging.debug(f"Checking {profile} {region}")
         try:
             client = session.client("ssm", region_name=region)
             resp = client.send_command(
-                InstanceIds=instance_ids,
+                InstanceIds=instance_ids if instanceid is None else instanceid,
                 DocumentName="AWS-CheckIfSqlServerRunning",
                 Parameters={
                     "commands": commands_to_run,
@@ -56,13 +49,14 @@ def do_something(session, instanceid, aws_region, profile):
                     InstanceId=instance_id
                 )
                 print(output)
+            
+            if instanceid is not None:
+                return instanceid
 
         except ClientError as e:
             error_code = e.response["Error"]["Code"]
             if error_code in ["AuthFailure", "UnrecognizedClientException"]:
                 logging.warning(f"Unable to process region {region}: {error_code}")
-            elif error_code == "AccessDenied":
-                logging.warning(f"Unable to process account {account_id}: {e}")
             elif error_code == "InvalidInstanceID.NotFound":
                 pass
             else:
@@ -79,18 +73,24 @@ def do_something(session, instanceid, aws_region, profile):
 @click.option("--instanceid", "-i", help="EC2 instance ID", default=None)
 @click.option("--region", "-r", help="AWS Region; use 'all' for all regions", default="ap-southeast-2")
 def main(profile, instanceid, region):
+    accounts_processed = []
     profile_names = [profile] if profile else aws_profiles
     
     for profile_name in profile_names:
         try:
             session = Session(profile_name=profile_name)
-            ret = do_something(session, instanceid, region, profile_name)
-            if ret is not None:
+            account_id = session.client("sts").get_caller_identity()["Account"]
+            if account_id in accounts_processed:
+                continue
+            accounts_processed.append(account_id)
+            
+            if do_something(session, profile_name, region, instanceid) is not None:
                 break
 
         except ClientError as e:
-            if e.response["Error"]["Code"] == "ExpiredToken":
-                logging.warning(f"{profile_name} token expired. Skipped")
+            error_code = e.response["Error"]["Code"]
+            if error_code in ["ExpiredToken", "AccessDenied"]:
+                logging.warning(f"{profile_name} {error_code}. Skipped")
             else:
                 raise
 
