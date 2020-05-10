619b3f8cf785307a5b34550bb80f6e9c2551fea4
Kay Hau <virtualda@gmail.com>
2020-05-10 15:18:43 +1000

    Added a simple template for quick scripting

---
+++ _base_file_templates/simple_aws_service_wrapper_2.py
@@ -0,0 +1,21 @@
+import click
+
+
+@click.group()
+def cli1():
+    pass
+
+
+@cli1.command(help="A command to print environment variables")
+@click.argument("envvar", type=click.STRING)
+def cmd1(envvar):
+    click.echo(f"Chosen envvar is {envvar}")
+
+
+@cli1.command(help="A command to do something with user")
+@click.argument("user", type=click.STRING)
+def cmd2(user):
+    click.echo(f"Chosen user is {user}")
+
+
+if __name__ == "__main__": cli1()
