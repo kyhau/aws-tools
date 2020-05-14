d23e608f599592b2139882b4e744f6b2613ed20f
Kay Hau <virtualda@gmail.com>
2020-05-15 09:45:56 +1000

    Updated awscli-v2 install scripts

---
+++ AWSCLI/Install-awscli-v2.ps1
@@ -0,0 +1,23 @@
+# Install awscli v2
+# https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-windows.html
+# Or
+# https://chocolatey.org/packages/awscli
+# (availability of latest version is a bit slower)
+# choco install awscli
+
+Write-Host "Downloading..."
+
+Invoke-WebRequest `
+  https://awscli.amazonaws.com/AWSCLIV2.msi `
+  -outfile AWSCLIV2.msi -UseBasicParsing
+
+Write-Host "Installing..."
+
+Start-Process msiexec.exe -Wait -ArgumentList '/I AWSCLIV2.msi /quiet'
+
+#Move-Item -Path saml2aws.exe -Destination C:\ProgramData\chocolatey\bin\saml2aws.exe -Force
+Remove-Item AWSCLIV2.msi -Force
+
+Write-Host "Removed AWSCLIV2.msi"
+
+aws --version
