e6e9b6c5eae795badc61170b54e9d8118026bc3a
Kay Hau <virtualda@gmail.com>
2020-06-02 19:52:41 +1000

    Updated lambda_deploy.py

--- Lambda/lambda_deploy.py
+++ Lambda/lambda_deploy.py
@@ -1,84 +1,72 @@
-import boto3
+from boto3.session import Session
 from botocore.exceptions import ClientError
 import click
+import datetime
+import json
 import logging
-from os.path import basename
+from time import mktime
 
-from arki_common.configs import (
-    init_wrapper,
-    default_config_file_path,
-)
-from arki_common.utils import check_response
-
-APP_NAME = basename(__file__).split(".")[0]
-
-# Default configuration file location
-DEFAULT_CONFIG_FILE = default_config_file_path(f"{APP_NAME}.toml")
-
-DEFAULT_CONFIGS = {
-    "aws.profile": {"required": False},
-    "aws.lambda.name": {"required": True},
-    "aws.lambda.runtime": {"required": True},
-    "aws.lambda.handler": {"required": True},
-    "aws.lambda.memory": {"required": True},
-    "aws.lambda.timeout": {"required": True},
-    "aws.lambda.tracingconfig": {"required": False, "default": "Active"},  # Active or PassThrough
-    "aws.lambda.role.arn": {"required": True},
-    "aws.lambda.environment": {"multilines": True},
-}
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("botocore").setLevel(logging.CRITICAL)
+logging.getLogger("boto3").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
 
 
-def prepare_func_configuration_args(settings, description):
-    """Prepare arguments for calling update_function_configuration()
-    """
-    # Retrieve and prepare arguments
-    func_configuration_args = {
-        "Description": description,
-        "FunctionName": settings["aws.lambda.name"],
-        "Handler": settings["aws.lambda.handler"],
-        "Role": settings["aws.lambda.role.arn"],
-        "Runtime": settings["aws.lambda.runtime"],
-        "MemorySize": int(settings["aws.lambda.memory"]),
-        "Timeout": int(settings["aws.lambda.timeout"]),
-        "TracingConfig": {"Mode": settings["aws.lambda.tracingconfig"]},
-    }
-
-    kms_key_arn = settings.get("aws.lambda.kmskey.arn")
-    if kms_key_arn is not None:
-        func_configuration_args["KMSKeyArn"] = kms_key_arn
-
-    # Retrieve lambda environment variables in dict form
-    env_vars = settings["aws.lambda.environment"]
-    if len(env_vars) > 0:
-        func_configuration_args["Environment"] = {"Variables": env_vars}
-
-    return func_configuration_args
+class DefaultEncoder(json.JSONEncoder):
+    """Encode for the json"""
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return int(mktime(obj.timetuple()))
+        return json.JSONEncoder.default(self, obj)
 
 
-def _lambda_deploy(settings, zipfile, description):
+dump_json = lambda x: json.dumps(x, cls=DefaultEncoder, sort_keys=True, indent=2)
 
-    lambda_function = settings["aws.lambda.name"]
-    lambda_alias = settings.get("aws.lambda.alias")
-    func_configuration_args = prepare_func_configuration_args(settings, description)
 
-    logging.info(f"Deploying (alias={lambda_alias}): {func_configuration_args}")
+def check_response(resp):
+    """Check response and log message if needed"""
+    ret = resp["ResponseMetadata"]["HTTPStatusCode"] in [200, 201, 202]
+    if ret is False:
+        logging.error(f"Response:\n{dump_json(resp)}")
+    return ret
 
-    with open(zipfile, mode="rb") as fh:
+
+def read_function_configuration(config_file):
+    with open(config_file, "r") as input_file:
+        data = input_file.read()
+        return json.loads(data)
+
+
+def _lambda_deploy(session, region, function_name, config_file, zip_file, description, lambda_alias):
+    client = session.client("lambda", region)
+
+    if config_file:
+        # Read configuration from config file
+        function_configuration = read_function_configuration(config_file)
+        function_configuration["Description"] = description
+    
+        if function_name != function_configuration.get("FunctionName"):
+            raise ValueError(
+                f"Function name not matched: arg({function_name}), "
+                f"config file ({function_configuration.get('FunctionName')}))")
+        logging.info(f"Deploying {function_name}:{lambda_alias}): {function_configuration}")
+    
+        # Update function configuration
+        resp = client.update_function_configuration(**function_configuration)
+        check_response(resp)
+        logging.info(f"Updated function config: Version={resp['Version']}, RevisionId={resp['RevisionId']}")
+
+    # Update function code
+    with open(zip_file, mode="rb") as fh:
         byte_stream = fh.read()
 
-    profile_name = settings.get("aws.profile")
-    if profile_name is not None:
-        boto3.setup_default_session(profile_name=profile_name)
+    if not byte_stream:
+        return
 
-    client = boto3.client("lambda", settings["aws.lambda.region"])
-
-    resp = client.update_function_configuration(**func_configuration_args)
-    check_response(resp)
-    logging.info(f"Updated function config: Version={resp['Version']}, RevisionId={resp['RevisionId']}")
-
+    # Update function code
     resp = client.update_function_code(
-        FunctionName=lambda_function,
-        ZipFile=byte_stream,
+        FunctionName=function_name,
+        zip_file=byte_stream,
         Publish=True
     )
     check_response(resp)
@@ -88,9 +76,10 @@ def _lambda_deploy(settings, zipfile, description):
     if lambda_alias is None:
         return
 
+    # Update function alias
     alias_exists = True
     try:
-        client.get_alias(FunctionName=lambda_function, Name=lambda_alias)
+        client.get_alias(FunctionName=function_name, Name=lambda_alias)
     except ClientError as e:
         if e.response["Error"]["Code"] == "ResourceNotFoundException":
             alias_exists = False
@@ -99,14 +88,14 @@ def _lambda_deploy(settings, zipfile, description):
 
     if alias_exists is True:
         resp = client.update_alias(
-            FunctionName=lambda_function,
+            FunctionName=function_name,
             Name=lambda_alias,
             FunctionVersion=version,
             Description=description,
         )
     else:
         resp = client.create_alias(
-            FunctionName=lambda_function,
+            FunctionName=function_name,
             Name=lambda_alias,
             FunctionVersion=version,
             Description=description,
@@ -115,49 +104,60 @@ def _lambda_deploy(settings, zipfile, description):
     logging.info(f"Updated alias on Version={resp['FunctionVersion']}: AliasArn={resp['AliasArn']}")
 
 
-@init_wrapper
-def process(*args, **kwargs):
-    logging.debug("At process")
-    logging.debug(kwargs)
-
-    try:
-        if kwargs.get("is_init"):
-            return 0
-        
-        settings = kwargs.get("_arki_settings")
-        zipfile = kwargs.get("zipfile")
-        description = kwargs.get("description")
-
-        _lambda_deploy(
-            settings=settings,
-            zipfile=zipfile,
-            description=description
-        )
-
-    except Exception as e:
-        logging.error(e)
-        return 1
-
-    return 0
+@click.group(invoke_without_command=True, help="List name of lambda functions if no command specified")
+@click.option("--profile", "-p", default="default", help="AWS profile name")
+@click.option("--region", "-r", default="ap-southeast-2", help="AWS region")
+@click.pass_context
+def cli_main(ctx, profile, region):
+    ctx.obj["session"] = Session(profile_name=profile)
+    ctx.obj["region"] = region
+    if ctx.invoked_subcommand is None:
+        logging.info("List functions")
+        client = ctx.obj["session"].client("lambda", region)
+        paginator = client.get_paginator("list_functions")
+        for page in paginator.paginate():
+            for f in page["Functions"]:
+                print(f["FunctionName"])
 
 
-@click.command()
-@click.argument("config_file", required=False, default=DEFAULT_CONFIG_FILE)
-@click.option("--init", "-i", is_flag=True)
-@click.option("--config_section", "-s", required=False, default=APP_NAME, help=f"E.g. {APP_NAME}.staging")
-@click.option("--zipfile", "-z", required=False, help="Zip file of the lambda function code")
-@click.option("--description", "-d", required=False, help="Description of the deployment")
-def lambda_deploy(config_file, init, config_section, zipfile, description):
-
-    process(
-        app_name=APP_NAME,
-        config_file=config_file,
-        is_init=init,
-        default_configs=DEFAULT_CONFIGS,
-        config_section=config_section,
-        zipfile=zipfile,
-        description=description,
-    )
+@cli_main.command(help="Deploy a lambda function")
+@click.argument("function_name", required=True)
+@click.option("--config-file", "-c", required=False, help="Json file containing lambda function configuration")
+@click.option("--zip-file", "-z", required=False, help="Zip file of the lambda function code")
+@click.option("--description", "-d", default="", help="Description of the deployment")
+@click.option("--alias", "-a", required=False, help="Function Alias")
+@click.pass_context
+def deploy(ctx, function_name, zip_file, config_file, description, alias):
+    logging.debug(f"deploy: {ctx.obj}")
+    session, region = ctx.obj["session"], ctx.obj["region"]
+    _lambda_deploy(session, region, function_name, config_file, zip_file, description, alias)
 
 
-if __name__ == "__main__": lambda_deploy()
+@cli_main.command(help="Export function configuration of a Lambda function")
+@click.option("--function-name", "-f", help="Function name", required=True)
+@click.option("--qualifier", "-q", help="Qualifier")
+@click.option("--output-file", "-o", help="Output file", default="config.json")
+@click.pass_context
+def export_config(ctx, function_name, qualifier, output_file):
+    logging.debug(f"export_function_configuration: {ctx.obj}")
+    session, region = ctx.obj["session"], ctx.obj["region"]
+    
+    parameters = {"FunctionName": function_name}
+    if qualifier:
+        parameters["Qualifier"] = qualifier
+    
+    client = session.client("lambda", region)
+    resp = client.get_function_configuration(**parameters)
+    check_response(resp)
+    for d in [
+        "CodeSha256", "CodeSize", "FunctionArn", "ResponseMetadata", "LastModified", "LastUpdateStatus",
+        "State", "Version",
+    ]:
+        del resp[d]
+    
+    with open(output_file, "w") as outfile:
+        json.dump(resp, outfile, indent=2)
+
+
+if __name__ == "__main__":
+    cli_main(obj={})
