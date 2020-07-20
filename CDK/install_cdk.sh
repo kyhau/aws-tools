36b679c1b77146404518e63ec865e04ec7c1ca1e
Kay Hau <virtualda@gmail.com>
2020-07-20 20:28:19 +1000

    Updated install_cdk.sh

--- CDK/install_cdk.sh
+++ CDK/install_cdk.sh
@@ -4,13 +4,14 @@ set -e
 
 # Prerequisites
 # - Node.js (>= 10.3.0)
+sudo apt-get install -y nodejs
 echo "node version: $(node --version)"
 echo "npm version: $(npm --version)"
 
 # Optional: useful for cdk-patterns/serverless
 #npm config set registry http://registry.npmjs.org/
-echo "npx version: $(npx --version)"
 sudo npm install -g npx
+echo "npx version: $(npx --version)"
 
 #echo "Uninstalling the AWS CDK.
 #sudo npm uninstall -g aws-cdk
---
+++ Useful-tools/nodejs/update_npm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://stackoverflow.com/questions/6237295/how-can-i-update-nodejs-and-npm-to-the-next-versions
+
+sudo npm install npm@latest -g
