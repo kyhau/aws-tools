527c83de34e9e5825ea7a485e619d91cb6ea2777
Kay Hau <virtualda@gmail.com>
2020-07-29 13:47:03 +1000

    Updated create_key_pair.sh

--- EC2-VPC/create_key_pair.sh
+++ EC2-VPC/create_key_pair.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
+KEY_NAME="MyKeyPair"
+
+aws ec2 create-key-pair --key-name ${KEY_NAME} --query 'KeyMaterial' --output text > ${KEY_NAME}.pem
+
 chmod 400 MyKeyPair.pem
