a1574f9366640c310ffe6ea9e4a7de719801e83e
Kay Hau <virtualda@gmail.com>
2020-08-01 11:35:21 +1000

    Removed some base files, use vscode snippets instead

--- _base_file_templates/lambda.md
+++
@@ -1,1 +0,0 @@
-See [Lambda/templates](../Lambda/templates/)
--- _base_file_templates/simple.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set to fail script if any command fails.
-set -e
-
-AWS_PROFILE="default"
-AWS_DEFAULT_REGION="ap-southeast-2"
-
