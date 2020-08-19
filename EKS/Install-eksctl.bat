1136980ffa23a6b0a2b002150c4ea88884343637
Kay Hau <virtualda@gmail.com>
2020-08-19 21:35:19 +1000

    Updated install_eksctl.sh

---
+++ EKS/Install-eksctl.bat
@@ -0,0 +1,2 @@
+choco install -y eksctl
+eksctl version
--- EKS/install_eksctl.sh
+++ EKS/install_eksctl.sh
@@ -7,9 +7,8 @@ set -e
 echo "Downloading the latest version of eksctl..."
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
-echo "Coping binary to .local/bin..."
-mkdir -p ~/.local/bin
-mv /tmp/eksctl ~/.local/bin/
+echo "Moving the extracted binary to /usr/local/bin..."
+sudo mv /tmp/eksctl /usr/local/bin
 
 echo "Checking version..."
 eksctl version
