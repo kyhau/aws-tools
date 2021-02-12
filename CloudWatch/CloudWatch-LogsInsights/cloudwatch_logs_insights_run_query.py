9cbc7ab752aebb241c132c7e55b74c0fe7a904d0
Kay Hau <virtualda@gmail.com>
2021-02-12 21:32:05 +1100

    convert the query time from local to utc

--- CloudWatch/CloudWatch-LogsInsights/cloudwatch_logs_insights_run_query.py
+++ CloudWatch/CloudWatch-LogsInsights/cloudwatch_logs_insights_run_query.py
@@ -1,9 +1,10 @@
-from boto3.session import Session
-import click
-from datetime import datetime, timedelta
 import json
 import logging
+from datetime import datetime, timedelta
+
+import click
 import yaml
+from boto3.session import Session
 
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -35,13 +36,19 @@ def main(profile, loggroupname, starttime, endtime, queryfile, region):
 
     end_dt = datetime.now() if endtime is None else datetime.strptime(endtime, "%Y-%m-%d")
     start_dt = end_dt - timedelta(hours=LOOKUP_HOURS) if starttime is None else datetime.strptime(starttime, "%Y-%m-%d")
-    
+
+     # Convert local datetime to UTC
+    local_to_utc = lambda dt: datetime.utcfromtimestamp(datetime.timestamp(dt))
+
+    end_dt = local_to_utc(end_dt)
+    start_dt = local_to_utc(start_dt)
+
     query_string = read_file(queryfile) if queryfile else default_query_string
-    
+
     query_id = client.start_query(
         logGroupName=loggroupname,
-        startTime=int(start_dt.timestamp()),
-        endTime=int(end_dt.timestamp()),
+        startTime=int(start_dt),
+        endTime=int(end_dt),
         queryString=query_string,
     )["queryId"]
 
