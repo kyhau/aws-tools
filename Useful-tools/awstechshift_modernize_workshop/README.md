82aa1cdc14cd3d874d720ba3cf4e4eb951617634
Kay Hau <virtualda+github@gmail.com>
2021-06-06 18:55:31 +1000

    Minor cleanup

--- Useful-tools/awstechshift_modernize_workshop/README.md
+++
@@ -1,4 +0,0 @@
-# Sample templates from AWS TechShift Modernize 2020
-
-[infra-lab8.yml](infra-lab8.yml)
-![drawing](arch8-big.png)
--- Useful-tools/awstechshift_modernize_workshop/k_lab3_codecommit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set to fail script if any command fails.
-set -e
-
-aws codecommit create-repository --repository-name TSAGallery-SPA --region ap-southeast-1
-
-aws codecommit create-repository --repository-name TSAGallery-API --region ap-southeast-1
-
