ba9e705abe0afec009bc889fdb21c53c14ad266a
Kay Hau <virtualda@gmail.com>
2021-06-24 12:57:59 +1000

    Updated list_securityhub_standard_controls.py
    Updated to support --detailed mode

--- SecurityHub/list_securityhub_standard_controls.py
+++ SecurityHub/list_securityhub_standard_controls.py
@@ -1,3 +1,8 @@
+"""
+Print current control status of Security Hub.
+- Print "ControlId: ControlStatus" of a standard control by default.
+- Print all details when `--detailed` is used.
+"""
 import json
 import logging
 
@@ -14,9 +19,10 @@ STANDARD_SUBSCRIPTIONS = {
 
 
 class Helper(AwsApiHelper):
-    def __init__(self, subscription):
+    def __init__(self, subscription, detailed):
         super().__init__()
         self._subscription = subscription
+        self._detailed = detailed
 
     def process_request(self, session, account_id, region, kwargs):
         kwargs["StandardsSubscriptionArn"] = f"arn:aws:securityhub:{region}:{account_id}:subscription/{self._subscription}"
@@ -25,22 +31,29 @@ class Helper(AwsApiHelper):
         resp = client.describe_standards_controls(**kwargs)
         next_token = resp.get("NextToken")
         for item in resp["Controls"]:
-            print(json.dumps(item, indent=2, default=str))
+            self.printline(item)
 
         while next_token:
             kwargs["NextToken"] = next_token
             resp = client.describe_standards_controls(Expression=self._sql_statement)
             next_token = resp.get("NextToken")
             for item in resp["Controls"]:
-                print(json.dumps(item, indent=2, default=str))
+                self.printline(item)
+
+    def printline(self, item):
+        if self._detailed:
+            print(json.dumps(item, indent=2, default=str))
+        else:
+            print(f"{item['ControlId']}: {item['ControlStatus']}")
 
 
 @click.command()
+@click.option("--detailed", "-d", show_default=True, is_flag=True)
 @click.option('--subscription', required=True, type=click.Choice(list(STANDARD_SUBSCRIPTIONS.keys()), case_sensitive=False))
 @click.option("--profile", "-p", help="AWS profile name. Use profiles in ~/.aws if not specified.")
 @click.option("--region", "-r", default="ap-southeast-2", show_default=True, help="AWS Region. Use 'all' for all regions.")
-def main(subscription, profile, region):
-    Helper(STANDARD_SUBSCRIPTIONS[subscription]).start(profile, region, "securityhub", kwargs={})
+def main(detailed, subscription, profile, region):
+    Helper(STANDARD_SUBSCRIPTIONS[subscription], detailed).start(profile, region, "securityhub", kwargs={})
 
 
 if __name__ == "__main__":
