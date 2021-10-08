02f9626a44b13f880423173b5330c955fe77160b
Kay Hau <virtualda@gmail.com>
2021-10-08 16:41:50 +1100

    Updated EKS/install_eksctl.sh

--- EKS/install_eksctl.sh
+++ EKS/install_eksctl.sh
@@ -4,12 +4,12 @@
 
 set -e
 
-echo "Downloading the latest version of eksctl..."
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+echo "INFO: Downloading eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C .
 
-echo "Moving the extracted binary to ~/.local/bin..."
+echo "INFO: Moving binary to ~/.local/bin"
 mkdir -p ~/.local/bin
-mv /tmp/eksctl ~/.local/bin/
+mv eksctl ~/.local/bin/
 
-echo "Checking version..."
+echo "INFO: Checking version"
 echo "eksctl version: $(eksctl version)"
