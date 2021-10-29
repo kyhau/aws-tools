518ace95b8c29d6688df247163457f16105216c0
Kay Hau <virtualda@gmail.com>
2021-10-29 13:06:03 +1100

    Updated install sam-cli and sam-cli-beta-cdk scripts (#105)

--- SAM-and-ServerlessApplicationRepository/install-sam-cli-beta-cdk.sh
+++ SAM-and-ServerlessApplicationRepository/install-sam-cli-beta-cdk.sh
@@ -3,7 +3,7 @@ set -e
 # See https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-cdk-getting-started.html
 # See https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install-linux.html
 
-# Download sam-cli-beta-cdk
+echo "INFO: Downloading sam-cli-beta-cdk"
 wget https://github.com/aws/aws-sam-cli/releases/download/sam-cli-beta-cdk/aws-sam-cli-linux-x86_64.zip
 
 # Verify the integrity and authenticity of the downloaded installer files by generating a hash value using the following command:
@@ -12,11 +12,17 @@ sha256sum aws-sam-cli-linux-x86_64.zip
 # Unzip the installation files into the sam-installation/ subdirectory.
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 
-# Install the AWS SAM CLI.
-sudo ./sam-installation/install --update
+if [[ -d "/usr/local/aws-sam-cli-beta-cdk/current" ]]
+then
+  echo "INFO: Updating sam-beta-cdk"
+  sudo ./sam-installation/install --update
+else
+  echo "INFO: Installing sam-beta-cdk"
+  sudo ./sam-installation/install
+fi
 
-# Verify the installation.
-sam-beta-cdk --version
+echo "INFO: Checking version"
+echo "sam-beta-cdk version: $(sam-beta-cdk --version)"
 
 rm -rf sam-installation
 rm aws-sam-cli-linux-x86_64.zip
--- SAM-and-ServerlessApplicationRepository/install-sam-cli.sh
+++ SAM-and-ServerlessApplicationRepository/install-sam-cli.sh
@@ -2,7 +2,11 @@
 set -e
 # See https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install-linux.html
 
-# Download latest version
+# AWS SAM Accelerate
+# To use the AWS SAM Accelerate, you must install version 1.34.1 or greater of the AWS SAM CLI
+# https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/accelerate-getting-started.html
+
+echo "INFO: Downloading aws-sam-cli"
 wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
 
 # Verify the integrity and authenticity of the downloaded installer files by generating a hash value using the following command:
@@ -11,11 +15,17 @@ sha256sum aws-sam-cli-linux-x86_64.zip
 # Unzip the installation files into the sam-installation/ subdirectory.
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 
-# Install the AWS SAM CLI.
-sudo ./sam-installation/install
+if [[ -d "/usr/local/aws-sam-cli/current" ]]
+then
+  echo "INFO: Updating sam"
+  sudo ./sam-installation/install --update
+else
+  echo "INFO: Installing sam"
+  sudo ./sam-installation/install
+fi
 
-# Verify the installation.
-sam --version
+echo "INFO: Checking version"
+echo "sam version: $(sam --version)"
 
 rm -rf sam-installation
 rm aws-sam-cli-linux-x86_64.zip
