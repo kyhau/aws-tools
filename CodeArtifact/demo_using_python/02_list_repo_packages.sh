dbec4b2683257379361a30a8e7c05c0f0b9c9269
kyhau <virtualda@gmail.com>
2022-01-27 19:50:12 +1100

    Updated CodeArtifact/demo_using_python/02_list_repo_packages.sh

--- CodeArtifact/demo_using_python/02_list_repo_packages.sh
+++ CodeArtifact/demo_using_python/02_list_repo_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://docs.aws.amazon.com/codeartifact/latest/ug/domain-overview.html
+# https://docs.aws.amazon.com/codeartifact/latest/ug/list-repos.html
 
 source .config
 
@@ -13,3 +14,11 @@ aws codeartifact list-packages --domain ${DOMAIN} --repository ${SHARED_REPO_2} 
 
 aws codeartifact list-packages --domain ${DOMAIN} --domain-owner ${ACCOUNT_1_ID} --repository ${SHARED_REPO_1} --profile ${AWS_PROFILE_2}
 aws codeartifact list-packages --domain ${DOMAIN} --domain-owner ${ACCOUNT_1_ID} --repository ${SHARED_REPO_2} --profile ${AWS_PROFILE_2}
+
+
+# List repositories in the domain
+
+aws codeartifact list-repositories-in-domain --domain ${DOMAIN} --domain-owner ${ACCOUNT_1_ID}
+
+# administratorAccount - The AWS account ID that manages the repository.
+# domainOwner - The 12-digit account number of the AWS account that owns the domain. It does not include dashes or spaces.
