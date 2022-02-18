a5fe5567582d59ff2801acb944b6ebc14d09ad6b
Kay Hau <virtualda@gmail.com>
2022-02-18 19:38:27 +1100

    Updated git-secrets install script

--- Useful-tools/git-secrets/install-git-secrets.sh
+++ Useful-tools/git-secrets/install-git-secrets.sh
@@ -4,18 +4,19 @@ set -e
 
 INSTALL_DIR="$HOME/workspaces/github"
 
-pushd $INSTALL_DIR
+ori_dir=$(pwd)
+
+cd $INSTALL_DIR
 
 if [[ ! -d "git-secrets" ]]; then
   git clone https://github.com/awslabs/git-secrets
 fi
 
-pushd git-secrets
+cd git-secrets
 git pull
 sudo make install
 
-popd
-popd
+cd ${ori_dir}
 
 # Add a configuration template if you want to add hooks to all repositories you initialize or clone in the future.
 git secrets --register-aws --global
