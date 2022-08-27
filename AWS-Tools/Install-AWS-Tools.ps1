8678536f8e4ce80e7c483c05894627f638f62b2d
Kay Hau <virtualda@gmail.com>
2022-08-27 20:10:02 +1000

    Refactored workflows

--- AWS-Tools/Install-AWS-Tools.ps1
+++ AWS-Tools/Install-AWS-Tools.ps1
@@ -1,5 +1,12 @@
 # https://www.powershellgallery.com/packages/AWS.Tools.Installer
+
+# Recommend that you don't run PowerShell as an administrator with elevated permissions except when required by
+# the task at hand. This is because of the potential security risk and is inconsistent with the principle of least
+# privilege.
+
+#Install-Module -Name AWS.Tools.Installer -Scope CurrentUser -Force
 Install-Module -Name AWS.Tools.Installer
+
 Install-Module -Name AWSPowerShell
 
 Install-Module -Name AWS.Tools.Common
