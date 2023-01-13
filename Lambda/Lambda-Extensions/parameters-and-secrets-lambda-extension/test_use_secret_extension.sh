c44da6b769aa946377da438fd6d30e7b2b7cc533
Kay Hau <virtualda@gmail.com>
2023-01-13 15:18:36 +1100

    Added test_use_secret_extension.sh use_secret_extension.py

--- Lambda/Lambda-Extensions/parameters-and-secrets-lambda-extension/test_use_secret_extension.sh
+++ Lambda/Lambda-Extensions/parameters-and-secrets-lambda-extension/test_use_secret_extension.sh
@@ -13,7 +13,7 @@ zip use_secret_extension.zip use_secret_extension.py
 # arn:aws:lambda:ap-southeast-2:665172237481:layer:AWS-Parameters-and-Secrets-Lambda-Extension-Arm64
 
 aws lambda update-function-configuration \
-    --function-name my-function \
+    --function-name use_secret_extension \
     --layers arn:aws:lambda:ap-southeast-2:665172237481:layer:AWS-Parameters-and-Secrets-Lambda-Extension
 
 rm use_secret_extension.zip
