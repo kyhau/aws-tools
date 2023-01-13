3d7136479a99173941f63e28836c0dc7f1331d27
Kay Hau <virtualda@gmail.com>
2023-01-13 15:18:00 +1100

    Added test_use_secret_extension.sh use_secret_extension.py

---
+++ Lambda/Lambda-Extensions/parameters-and-secrets-lambda-extension/test_use_secret_extension.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html
+
+cdk init app --language python
+
+aws secretsmanager create-secret --name MyTestSecret --secret-string HelloWorldTestCase1
+
+zip use_secret_extension.zip use_secret_extension.py
+
+# arn:aws:lambda:ap-southeast-2:665172237481:layer:AWS-Parameters-and-Secrets-Lambda-Extension
+# arn:aws:lambda:ap-southeast-2:665172237481:layer:AWS-Parameters-and-Secrets-Lambda-Extension-Arm64
+
+aws lambda update-function-configuration \
+    --function-name my-function \
+    --layers arn:aws:lambda:ap-southeast-2:665172237481:layer:AWS-Parameters-and-Secrets-Lambda-Extension
+
+rm use_secret_extension.zip
---
+++ Lambda/Lambda-Extensions/parameters-and-secrets-lambda-extension/use_secret_extension.py
@@ -0,0 +1,33 @@
+import json
+import urllib.parse
+from os import environ
+
+import requests
+
+# from aws_xray_sdk.core import patch_all, xray_recorder
+# patch_all()
+
+HEADERS = {"X-Aws-Parameters-Secrets-Token": environ.get("AWS_SESSION_TOKEN")}
+SECRET_NAME = "MyTestSecret"
+SECRET_PORT = environ.get("PARAMETERS_SECRETS_EXTENSION_HTTP_PORT")
+
+
+def get_lambda_response(status_code: int, body: str):
+    return {
+        "statusCode": status_code,
+        "body": body
+    }
+
+
+def get_secret(secret_id: str, version_stage: str=None) -> str:
+    request_uri = f"http://localhost:{SECRET_PORT}/secretsmanager/get?secretId={urllib.parse.quote(secret_id)}"
+    if version_stage:
+        request_uri = f"{request_uri}&versionStage={urllib.parse.quote(version_stage)}"
+
+    response = requests.get(url=request_uri, headers=HEADERS)
+    return response.json()["SecretString"]
+
+
+def lambda_handler(event, context):
+    print(f"Retrieved value: {get_secret(secret_id=SECRET_NAME)}")
+    return get_lambda_response(200, json.dumps("Hello from Lambda!"))
