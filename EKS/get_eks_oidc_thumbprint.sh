ec1795f0a8a7ef62aa08061e9adc116598ed8a4b
Kay Hau <virtualda@gmail.com>
2023-02-15 13:10:50 +1100

    Updated EKS/get_eks_oidc_thumbprint.sh

--- EKS/get_eks_oidc_thumbprint.sh
+++ EKS/get_eks_oidc_thumbprint.sh
@@ -9,11 +9,11 @@ REGIONS="us-east-2
     us-east-1
     us-west-1
     us-west-2
-    ap-northeast-1
     ap-southeast-1
     ap-southeast-2
     ap-southeast-3
     ap-southeast-4
+    ap-northeast-1
     eu-central-1
     eu-west-1
     eu-west-2
