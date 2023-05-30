b8c1c1d75555ca7b11aebfe25f2ccb691a854072
Kay Hau <virtualda@gmail.com>
2023-05-30 15:23:04 +1000

    Updated

---
+++ CodeArtifact/demo_using_python/21_account2_upload_package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+source .env
+
+# Download a wheel
+# mkdir dist
+# cd dist
+# pip wheel boto3==1.20.35
+# cd ..
+
+# pip install -i https://pypi.org/simple twine
+
+aws codeartifact login --tool twine --domain ${DOMAIN} --domain-owner ${ACCOUNT_2_ID} \
+   --repository ${ACCOUNT_2_REPO_1} --profile ${AWS_PROFILE_2}
+cat ~/.pypirc
+
+twine upload --repository codeartifact dist/boto3-1.20.35-py3-none-any.whl
+
+aws codeartifact list-package-versions --domain ${DOMAIN} --domain-owner ${ACCOUNT_2_ID} \
+  --repository ${ACCOUNT_2_REPO_1} --format pypi --package boto3 --profile ${AWS_PROFILE_2}
