40cb39a0c42276b707a58d7cbf1291b9fc6a5c54
Kay Hau <virtualda@gmail.com>
2023-05-31 13:50:14 +1000

    Updated

--- CodeArtifact/demo_using_python/21_account2_upload_package.sh
+++ CodeArtifact/demo_using_python/21_account2_upload_package.sh
@@ -7,6 +7,7 @@ source .env
 # mkdir dist
 # cd dist
 # pip wheel boto3==1.20.35
+# pip wheel cdk-monitoring-constructs==4.0.9
 # cd ..
 
 # pip install -i https://pypi.org/simple twine
@@ -15,7 +16,8 @@ aws codeartifact login --tool twine --domain ${DOMAIN} --domain-owner ${ACCOUNT_
    --repository ${ACCOUNT_2_REPO_1} --profile ${AWS_PROFILE_2}
 cat ~/.pypirc
 
-twine upload --repository codeartifact dist/boto3-1.20.35-py3-none-any.whl
+# twine upload --repository codeartifact dist/boto3-1.20.35-py3-none-any.whl
+twine upload --repository codeartifact dist/cdk_monitoring_constructs-4.0.9-py3-none-any.whl
 
 aws codeartifact list-package-versions --domain ${DOMAIN} --domain-owner ${ACCOUNT_2_ID} \
   --repository ${ACCOUNT_2_REPO_1} --format pypi --package boto3 --profile ${AWS_PROFILE_2}
