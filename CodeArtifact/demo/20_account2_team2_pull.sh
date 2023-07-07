9bf4a069c680000b721cba889cac9e78783f41dc
Kay Hau <virtualda@gmail.com>
2023-07-07 14:23:01 +1000

    updated

--- CodeArtifact/demo/20_account2_team2_pull.sh
+++ CodeArtifact/demo/20_account2_team2_pull.sh
@@ -3,13 +3,8 @@ set -e
 
 source .env
 
-# Access a domain owned by an account you are authenticated to:
-
 AWS_PROFILE=${AWS_PROFILE_2}
 REPO_NAME=${ACCOUNT_2_TEAM_2_REPO}
-
-# aws codeartifact list-package-versions --domain ${DOMAIN} --domain-owner ${ACCOUNT_2_ID} \
-#  --repository ${REPO_NAME} --format pypi --package boto3 --profile ${AWS_PROFILE_2}
 
 aws codeartifact login --tool pip --domain ${DOMAIN} --domain-owner ${ACCOUNT_1_ID} \
   --repository ${REPO_NAME} --profile ${AWS_PROFILE}
@@ -20,3 +15,6 @@ echo "CheckPt: pip3 install boto3"
 pip3 install boto3
 
 rm ~/.config/pip/pip.conf
+
+# Test - xx_list_packages.sh
+# Test - xx_list_packages_in_external_repos.sh
