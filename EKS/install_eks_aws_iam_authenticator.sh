ad64fa9a2f4b2a7623b7157578d9701942d8c202
Kay Hau <virtualda@gmail.com>
2023-10-10 10:21:28 +1100

    Updated install_eks_aws_iam_authenticator.sh

--- EKS/install_eks_aws_iam_authenticator.sh
+++ EKS/install_eks_aws_iam_authenticator.sh
@@ -10,7 +10,7 @@ else
   echo "INFO: aws-iam-authenticator not installed"
 fi
 
-VERSION=$(curl -Ls https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest | grep 'href="/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.[0-9]*.[0-9]*/aws-iam-authenticator_0.[0-9]*.[0-9]_linux_amd64' | awk -F '/' '{print $6}' | cut -c 2-)
+VERSION=$(curl -Ls https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest | grep 'href="/kubernetes-sigs/aws-iam-authenticator/releases/tag' | awk -F ' ' '{print $NF}' | cut -c 2-)
 
 echo "INFO: Downloading aws-iam-authenticator binary from Amazon S3"
 curl -L -o aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${VERSION}/aws-iam-authenticator_${VERSION}_linux_amd64
